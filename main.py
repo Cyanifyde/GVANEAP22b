ef2bd38d648b953ce62f3d3a3151b89d45b6f1ae
Cyan <81624686+Cyanifyde@users.noreply.github.com>
2023-01-13 15:08:27 +0000

    added files

---
+++ main.py
@@ -0,0 +1,405 @@
+
+import random
+import os
+import psutil
+import shutil
+import threading
+import math
+import sys
+#import matplotlib.pyplot as plt
+
+def quit():
+    pygame.quit()
+    sys.exit()
+
+def change_to_scene_1():
+    global switch
+    switch=1
+    object_list.remove_set(["runmain","exit"])
+    object_list.add_set([summonhouse,summonperson,startsim,simsettings])
+    object_list.add([tellformenu,tellformenu2])
+
+def change_to_scene_2():
+    global switch
+    switch=2
+    object_list.remove_all_set()
+    place_house()
+
+def change_to_scene_3():
+    check_file()
+    change_to_scene_1()
+
+def create_paths():
+    global path_store
+    path_store=path_store_set()
+    g=house_list.dict.keys()
+    g=list(g)
+    for i, x in enumerate(g):
+        x1=house_list.dict[x]
+        for y in g[i+1:]:
+            y2=house_list.dict[y]
+            path=get_path(x1.loc2,y2.loc2)
+            path_store.add([x1,y2],path)
+            
+def change_to_scene_5():
+    global switch
+    global group_people
+    group_people=pygame.sprite.Group()
+    global infectedlist
+    infectedlist=pygame.sprite.Group()
+    global noninfectedlist
+    noninfectedlist=pygame.sprite.Group()
+    switch=3
+    object_list.remove_all_set()
+    numcounter=button_summon()
+    numcounter.no_box("numcounter","0",[10,10])
+    object_list.add(numcounter)
+    infectcounter=button_summon()
+    infectcounter.no_box("infectcounter","0",[10,30])
+    object_list.add(infectcounter)
+    place_house()
+    global people_group
+    people_group=person_set(house_list)
+    create_paths()
+    for x in range(int(people_group.dict["initial_summon"])):
+        people_group.make()
+    people_group.infected=0
+    for x in people_group.people:
+        if x.dict["infected"]:
+            people_group.infected+=1
+            x.colour="red"
+            infectedlist.add(x)
+        else:
+            noninfectedlist.add(x)
+
+    
+
+def check_file():
+    os.startfile("person.txt")
+    running=True
+    while running==True:
+        running="notepad.exe" in (i.name() for i in psutil.process_iter())
+
+def back_to_menu():
+    global switch
+    switch=0
+    object_list.remove_set("menu")
+    object_list.add_set([runmain,exit])
+
+def create_place_house():
+    rect=house_list.make()
+    object_list.add(rect)
+
+def place_house():
+    for x in house_list.dict:
+        object_list.add(house_list.dict[x].obj)
+
+def get_random_bool(chance):
+    if random.random()<chance:
+        return True
+    else:
+        return False
+
+def check_collision():
+    for x in infectedlist.sprites():
+        blocks_hit_list = pygame.sprite.spritecollide(x, group_people, False, pygame.sprite.collide_circle)
+        for y in blocks_hit_list:
+            if y.dict["infected"]:
+                pass
+            else:
+                try:
+                    pygame.draw.line(screen,"black",x.path[0],y.path[0])
+                except:
+                    pass
+                chance=float(people_group.dict["infection_chance_spread"])
+                if get_random_bool(chance):
+                    if y.dict["infected"]==False:
+                        y.dict["infected"]=True
+                        y.change_colour("red")
+                        people_group.infected+=1
+                if get_random_bool(float(people_group.dict["die_chance"])):
+                    people_group.infected-=1
+                    try:
+                        people_group.people.remove(x)
+                    except:
+                        pass
+                elif get_random_bool(float(people_group.dict["get_better_chance"])):
+                    people_group.infected-=1
+                    x.dict["infected"]=False
+                    x.change_colour("blue")
+    
+
+                
+                    
+
+def move_check_new():
+    for x in (people_group.people):
+        if get_random_bool(float(people_group.dict["move_chance"])):
+            if x not in group_people.sprites():
+                y=house_list.dict[random.randint(1,len(house_list.dict))]
+                g=y.loc
+                if x.house.loc!=y.loc:
+                    x.dict["goto"]=[g[0]+5,g[1]+5]
+                    x.dict["inHouse"]=False
+                    x.dict["gotoHouse"]=y
+                    x.path=path_store.get_dir(x.house,y)
+                    x.house=y
+                    if x.dict["infected"]:
+                        infectedlist.add(x)
+                    else:
+                        noninfectedlist.add(x)
+                    group_people.add(x)
+
+
+def scene_main_menu(running,num,select_run):
+    num+=1
+    screen.fill("black")
+    if num>=len(image_of_house_run):
+        num=0
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+            num=0
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            select_run=False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            select_run=True
+            select.make()
+            buttons.check(buttons)
+
+    screen.blit(image_of_house_run[num], (0, 0))
+    for x in object_list.dict:
+        object_list.dict[x].draw(screen)
+    if select_run==True:
+        select.draw(screen)
+    
+    buttons.check_hover(buttons)
+
+    return [running,num,select_run]
+
+def scene_initialise(running,num,select_run):
+    
+    screen.fill("black")
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            quit()
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            select_run=False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            select_run=True
+            select.make()
+            buttons.check(buttons)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                object_list.remove_all_set()
+                object_list.add_set(menu)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                object_list.remove_all_set()
+                object_list.add_set([summonhouse,summonperson,simsettings,startsim])
+                object_list.add([tellformenu,tellformenu2])
+   
+    for x in object_list.dict:
+        object_list.dict[x].draw(screen)
+    if select_run==True:
+        select.draw(screen)
+
+    buttons.check_hover(buttons)
+
+    return [running,num,select_run]
+
+def scene_place_houses(running,num,select_run):
+     
+    screen.fill("white")
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            quit()
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            pass
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            create_place_house()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                select_run=False
+                change_to_scene_1()
+   
+    for x in object_list.dict:
+        object_list.dict[x].draw(screen)
+
+    buttons.check_hover(buttons)
+
+    return [running,num,select_run]
+
+
+from sys import getsizeof
+def scene_simulation(running,num,select_run,speed=5,num2=0,time_elapsed_since_last_action=0,values={},values2={}):
+    num+=1
+    num2+=1
+    
+    if num%speed==0 or speed==1:
+        screen.fill("white")
+        check_collision()
+        move_check_new()
+        group_people.update(0,screen)
+        num=0
+
+    #group_people.update(1,screen)
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                people_group.make()
+            if event.key == pygame.K_UP:
+                speed-=1
+            if event.key == pygame.K_DOWN:
+                speed+=1
+            if event.key == pygame.K_LEFT:
+                """plt.plot(values.keys(), values.values(),label = "line 1")
+                plt.plot(values2.keys(), values2.values(), label = "line 2")
+                plt.xlabel('x - axis')
+                plt.ylabel('y - axis')
+                plt.title('My first graph!')
+    
+                plt.show()"""
+
+    if speed<1:
+        speed=1
+    
+    for x in object_list.dict:
+        object_list.dict[x].draw(screen)
+    
+    dt = clock.get_time() 
+    num2+=time_elapsed_since_last_action
+    time_elapsed_since_last_action += dt
+    if time_elapsed_since_last_action > 200:
+
+        if people_group.infected<0:
+            people_group.infected=0
+        object_list.dict["numcounter"].change_text_nobox(str(len(people_group.people)))
+        object_list.dict["infectcounter"].change_text_nobox(str(people_group.infected))
+        
+        time_elapsed_since_last_action = 0
+        values[num2/10000]=len(people_group.people)
+        values2[num2/10000]=people_group.infected
+    return [running,num,select_run,speed,num2,time_elapsed_since_last_action,values,values2]
+
+import pygame
+from zurich import *
+
+pygame.init()
+pygame.display.set_caption('NEA')
+
+Icon = pygame.image.load('images/a.png')
+image = pygame.image.load('images/house1.png')
+main_image1 = pygame.transform.scale(image, (500, 500))
+image = pygame.image.load('images/house3.png')
+main_image3 = pygame.transform.scale(image, (500, 500))
+image = pygame.image.load('images/house5.png')
+main_image5 = pygame.transform.scale(image, (500, 500))
+image = pygame.image.load('images/house4.png')
+main_image4 = pygame.transform.scale(image, (500, 500))
+image = pygame.image.load('images/house2.png')
+main_image2 = pygame.transform.scale(image, (500, 500))
+image_of_house_run=[main_image1,main_image3,main_image5,main_image4,main_image2]
+from pygame.locals import FULLSCREEN,DOUBLEBUF,RESIZABLE,OPENGL,HWSURFACE
+pygame.display.set_icon(Icon)
+args=DOUBLEBUF |HWSURFACE 
+screen = pygame.display.set_mode([500,500],args,8)
+clock = pygame.time.Clock()
+time= pygame.time
+num=0
+
+select=selection()
+select_run=False
+
+object_list=objects()
+buttons=buttons_set()
+
+object_list.button_store(buttons)
+
+runmain=button_summon()
+runmain.make("runmain","blue","white",[250,130],"Run Simulation Startup",change_to_scene_1)
+
+exit=button_summon()
+exit.make("exit","blue","white",[250,160],"Quit",quit)
+
+object_list.add_set([exit,runmain])
+
+summonhouse=button_summon()
+summonhouse.make("summonhouse","blue","white",[80,20],"Place houses",change_to_scene_2)
+
+summonperson=button_summon()
+summonperson.make("summonperson","blue","white",[80,50],"Summon Person",change_to_scene_3)
+
+simsettings=button_summon()
+simsettings.make("simsettings","blue","white",[80,80],"Simulation settings",None)
+
+startsim=button_summon()
+startsim.make("startsim","blue","white",[80,110],"Start Simulation",change_to_scene_5)
+
+menu=button_summon()
+menu.make("menu","blue","white",[80,20],"Menu Screen",back_to_menu)
+
+goto_sim_menu=button_summon()
+goto_sim_menu.make("simmenu","blue","white",[80,20],"Simulation menu",change_to_scene_1)
+
+tellformenu=button_summon()
+tellformenu.make("tellformenu","blue","white",[80,140],"press escape",None)
+
+tellformenu2=button_summon()
+tellformenu2.make("tellformenu2","blue","white",[80,160],"to get menu",None)
+
+chnagefile=button_summon()
+chnagefile.make("chnagefile","blue","white",[250,130],"Change the items in the file, and save when done",None)
+
+house_list=house_set()
+
+args=[True,num,select_run]
+switch=0
+num=0
+running = True
+while running:
+    pygame.display.set_caption("Fps: " + str(int(clock.get_fps())))
+    if switch==0:
+        args=scene_main_menu(*args)
+        pygame.display.flip()
+        clock.tick(10)
+    if switch==1:
+        args=scene_initialise(*args)
+        pygame.display.flip()
+        clock.tick(50)
+    if switch==2:
+        args=scene_place_houses(*args)
+        pygame.display.flip()
+        clock.tick(50)
+
+    if switch==3:
+        args=scene_simulation(*args)
+        pygame.display.flip()
+        clock.tick()
+    num+=1
+
+    running= args[0]
---
+++ zurich.py
@@ -0,0 +1,383 @@
+
+import pygame, random
+
+
+
+class store():
+    def __init__(self):
+        self.dict={}
+    def add(self,obj):
+        if type(obj) is list:
+            for i in obj:
+                self.dict[i.tag]=i
+        else:
+            self.dict[obj.tag]=obj
+
+    def remove(self, obj):
+        try:
+            del self.dict[obj]
+        except:
+            pass
+
+    def remove_all(self):
+        self.dict={}
+
+
+        
+
+
+
+class objects(store):
+    def __init__(self):
+        super().__init__()
+        
+    def button_store(self,obj):
+        self.button_obj=obj
+
+    def remove_set(self,obj):
+        
+        if type(obj) is list:
+            for i in obj:
+                self.button_obj.remove(i)
+                self.remove(i)
+        else:
+            self.button_obj.remove(obj)
+            self.remove(obj)
+
+    def add_set(self,obj):
+
+        self.button_obj.add(obj)
+        self.add(obj)
+
+    def remove_all_set(self):
+        self.button_obj.remove_all()
+        return super().remove_all()
+
+class buttons_set(store):
+    def check(self,buttons):
+        pos=pygame.mouse.get_pos()
+        try:
+            for i in self.dict:
+                g=self.dict[i]
+                if pos[0]>=(g.boxx-g.textwidth) and pos[1]>=(g.boxy-g.textheight) and pos[0]<=(g.boxxwidth+g.boxx-g.textwidth) and pos[1]<=(g.boxy+g.boxyheight-g.textheight):
+                    buttons.dict[i].func()
+                    return True
+        except:pass
+    
+    def check_hover(self,buttons):
+        pos=pygame.mouse.get_pos()
+        for i in self.dict:
+            g=self.dict[i]
+            if pos[0]>=(g.boxx-g.textwidth) and pos[1]>=(g.boxy-g.textheight) and pos[0]<=(g.accwidth) and pos[1]<=(g.accheight):
+                buttons.dict[i].create("red")
+            else:
+                buttons.dict[i].create("blue")
+
+class circle_summon():
+    def __init__(self):
+        self.tag=None
+        self.radius=None
+        self.colour=None
+
+    def make(self,name,colour,loc):
+        self.tag=name
+        self.colour=colour
+        self.loc=loc
+    def set(self,loc):
+        self.loc=loc
+    
+    def draw_mouse(self,screen):
+        pygame.draw.circle(screen, self.colour , pygame.mouse.get_pos(), self.radius)
+    def draw(self,screen):
+        pygame.draw.circle(screen,self.colour,self.loc,5)
+
+
+
+class button_summon():
+    def __init__(self):
+        self.smallfont = pygame.font.SysFont('Corbel',20) 
+        self.tag=None
+
+    def make(self,name,buttoncolour,textcolour,textloc,text,func):
+        self.tag=name
+        self.buttoncolour=buttoncolour
+        self.textcolour=textcolour
+        self.textloc=textloc
+        text = self.smallfont.render(text , True , self.textcolour)
+        self.text=text
+        self.boxxwidth=text.get_width()+10
+        self.boxyheight=text.get_height()+5
+        self.textwidth=text.get_width()/2
+        self.textheight=text.get_height()/2
+        self.boxx=self.textloc[0]-6
+        self.boxy=self.textloc[1]-3
+        self.func=func
+        self.rectangle=rect_summon()
+        self.accwidth=self.boxxwidth+self.boxx-self.textwidth
+        self.accheight=self.boxy+self.boxyheight-self.textheight
+        self.create(self.buttoncolour)
+        self.nobox=False
+    def create(self,colour):
+        self.colour=colour
+        self.rectangle.make(self.tag,colour,[self.boxx-self.textwidth,self.boxy-self.textheight],self.boxxwidth,self.boxyheight)
+    def no_box(self,name,text,loc):
+        self.tag=name
+        self.nobox=True
+        self.loc=loc
+        self.textcolour="black"
+        self.smallfont = pygame.font.SysFont('arial',15) 
+        self.text = self.smallfont.render(text , True , self.textcolour)
+
+    def change_text_nobox(self,text):
+        self.text = self.smallfont.render(text , True , self.textcolour)
+    def draw(self,screen):
+        if self.nobox:
+            screen.blit(self.text , self.loc)
+        else:
+            self.rectangle.draw(screen)
+            screen.blit(self.text , [self.textloc[0]-self.textwidth,self.textloc[1]-self.textheight])
+
+class rect_summon():
+    def __init__(self):
+        self.tag=None
+        self.colour=None
+    def make(self,name,colour,loc,height,width):
+        self.tag=name
+        self.colour=colour
+        self.width=width
+        self.height=height
+        self.loc=loc
+    def draw(self,screen):
+        pygame.draw.rect(screen,self.colour,pygame.Rect(self.loc[0], self.loc[1], self.height, self.width))
+        
+class selection_summon():
+    def __init__(self):
+        self.tag=None
+        self.colour=None
+    def make(self,name,colour):
+        self.tag=name
+        self.colour=colour
+    def draw(self,loc1,loc2,screen):
+        loc1x=loc1[0]
+        loc1y=loc1[1]
+        loc2x=loc2[0]
+        loc2y=loc2[1]
+        if loc1x<loc2x:
+            if loc1y<loc2y:
+                s = pygame.Surface((loc2x-loc1x,loc2y-loc1y))
+                s.set_alpha(128)
+                s.fill((255,255,255))
+                screen.blit(s, (loc1x,loc1y))
+            else:
+                s = pygame.Surface((loc2x-loc1x,loc1y-loc2y))
+                s.set_alpha(128)
+                s.fill((255,255,255))
+                screen.blit(s, (loc1x,loc2y))
+        else:
+            if loc2y<loc1y:
+                s = pygame.Surface((loc1x-loc2x,loc1y-loc2y))
+                s.set_alpha(128)
+                s.fill((255,255,255))
+                screen.blit(s, (loc2x,loc2y))
+            else:
+                s = pygame.Surface((loc1x-loc2x,loc2y-loc1y))
+                s.set_alpha(128)
+                s.fill((255,255,255))
+                screen.blit(s, (loc2x,loc1y))
+
+class selection():
+    def __init__(self):
+        self.rect=selection_summon()
+        self.rect.make("selectionbox",(0,255,0))
+        self.current_pos=pygame.mouse.get_pos()
+        self.end_pos=None
+    def make(self):
+        self.start_pos=pygame.mouse.get_pos()
+    def draw(self,screen):
+        self.current_pos=pygame.mouse.get_pos()
+        self.rect.draw(self.start_pos,self.current_pos,screen)
+
+class house_set(store):
+    def make(self):
+        loc=pygame.mouse.get_pos()
+        loc=[loc[0]-5,loc[1]-5]
+        rectangle=rect_summon()
+        rectangle.make(len(self.dict)+1,"black",loc,10,10)
+        house_created=house_summon(len(self.dict)+1,loc,rectangle)
+        self.add(house_created)
+        return rectangle
+
+class house_summon(store):
+    def __init__(self,tag,loc,obj):
+        super().__init__()
+        self.tag=tag
+        self.loc=loc
+        self.loc2=[loc[0]+5,loc[1]+5]
+        self.obj=obj
+
+class person_set(store):
+    def __init__(self,housing):
+        super().__init__()
+        self.people=[]
+        self.moveinglist=[]
+        self.infected=0
+        self.housing=housing
+        f = open("person.txt", "r")
+        l=f.read().splitlines()
+        newlist=[]
+        newlist2={}
+        for x in l:
+            newlist.append(x.split(" ")[0])
+        for x in newlist:
+            newlist2[x.split("=")[0]]=x.split("=")[1]
+        self.dict=newlist2
+    def make(self):
+        if self.dict["spawn"]!="None":
+            house=self.housing.dict[random.randint(1,len(self.housing.dict))]
+        else:
+            house=None
+        vars=self.dict
+        person=person_summon(house,vars)
+        self.people.append(person)
+
+class person_summon(pygame.sprite.Sprite):
+    def __init__(self,house,vars,colour="blue") :
+        self.dict={}
+        self.tag=''.join(random.choice("aqwsderfgtyhjuiklopmnbvcxz") for i in range(20))
+        self.house=house
+        self.vars=vars
+        self.dict["infected"]=(random.random()<float(self.vars["infection_chance"]))
+        self.dict["inHouse"]=(self.vars["spawn"]!="None")
+        self.dict["goto"]=None
+        self.dict["gotoHouse"]=None
+        self.dict["loc"]=self.house.loc2
+        self.temppath=[]
+        self.width=self.dict["loc"][0]
+        self.height=self.dict["loc"][1]
+        self.radius=23
+        self.image = pygame.Surface([100, 100])
+        self.colour = colour
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.width, self.height)
+        self.image.fill("red")
+        
+        super().__init__()
+
+    def move(self,screen):
+        try:
+            self.draw_circ(screen)
+            self.image.fill("red")
+            self.rect.x=self.path[0][0]
+            self.rect.y=self.path[0][1]
+            del self.path[0]
+        except:
+            self.kill()
+
+    def update(self,num,screen):
+        if num==0:
+            self.move(screen)
+        else:
+            self.draw_circ(screen)
+
+    def draw_circ(self,screen):
+        try:
+            pygame.draw.circle(screen, self.colour, (self.path[0][0],self.path[0][1]), 5)
+        except:
+            self.kill()
+    def change_colour(self,colour):
+        self.colour=colour
+
+class person_summon2(store):
+    def __init__(self,house,vars,moveinglist):
+        super().__init__()
+        self.tag=''.join(random.choice("aqwsderfgtyhjuiklopmnbvcxz") for i in range(20))
+        self.house=house
+        self.vars=vars
+        self.circ=None
+        self.temppath=[]
+        self.moveinglist=moveinglist
+        self.setup()
+
+    def setup(self):
+        self.dict["infected"]=(random.random()<float(self.vars["infection_chance"]))
+        self.dict["inHouse"]=(self.vars["spawn"]!="None")
+        self.dict["goto"]=None
+        self.dict["gotoHouse"]=None
+        self.dict["loc"]=self.house.loc2
+        
+
+    def make(self):
+        g=circle_summon()
+        if self.dict["infected"]==True:
+            g.make(self.tag,"red",self.dict["loc"])
+        else:
+            g.make(self.tag,"blue",self.dict["loc"])
+        self.circ=g
+        return g
+
+    def draw(self,screen):
+        self.circ.draw(screen)
+    def change_colour(self,colour):
+        self.circ.colour=colour
+
+    def move(self):
+        try:
+            self.dict["loc"]=self.path[0]
+            self.make()
+            del self.path[0]
+        except:
+            self.moveinglist.remove(self)
+
+    def in_house(self):
+        return self.dict["inHouse"]
+
+
+
+
+def distance(start,end):
+    return ((start[0] - end[0]) ** 2) + ((start[1] - end[1]) ** 2)
+
+def get_path(start,end):
+    array=[]
+    adj=[[0, -1], [0, 1], [-1, 0], [1, 0],[-1, -1], [-1, 1], [1, 1], [1, -1]]
+    current=start
+    shortest=current
+    running=True
+    while running== True:
+        shortest_dist=distance(current,end)
+        for x in adj:
+            pos=[current[0]+x[0],current[1]+x[1]]
+            dist=distance(pos,end)
+            if shortest_dist>=dist:
+                shortest=pos
+                current=pos
+                shortest_dist=dist
+        if shortest==end:
+            running=False
+        array.append(shortest)
+    return array
+
+
+def switch_arr(arr):
+    new_arr=[]
+    for x in range(len(arr)):
+        g=x+1
+        new_arr.append(arr[-g])
+    new_arr.pop(0)
+    return new_arr
+
+class path_store_set(store):
+    def add(self,obj,obj1):
+        self.dict[str([obj[0].tag,obj[1].tag])]=[obj1,switch_arr(obj1)]
+    def get_dir(self,house1,house2):
+        if house1!=None and house2!=None :
+            for x in self.dict:
+                list_one= eval(x)
+                if house1.tag in list_one:
+                    if house2.tag in list_one:
+                        arr=self.dict[x]
+                        if list_one.index(house1.tag)==0:
+                            return arr[0].copy()
+                        if list_one.index(house2.tag)==0:
+                            return arr[1].copy()
+
